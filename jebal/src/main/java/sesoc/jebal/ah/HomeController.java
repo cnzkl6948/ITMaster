8e15fcfa616c0ac61099be399818e69237b228c4
SCITMASTER <SCITMASTER@LAPTOP-LLP5EV42>
2017-08-20 18:11:44 +0900

    밥 먹으러 가자

--- jebal/src/main/java/sesoc/jebal/ah/HomeController.java
+++ jebal/src/main/java/sesoc/jebal/ah/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("밥 먹으러 가자");
 		return "home";
 	}
 	
